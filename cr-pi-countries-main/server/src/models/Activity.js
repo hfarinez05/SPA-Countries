9e62fea98c9bf7c1130a42c9bd05d313cda8b5e1
hfarinez05 <hfarinez05@gmail.com>
2023-12-12 02:42:35 -0300

    Primer commit

---
+++ cr-pi-countries-main/server/src/models/Activity.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("Activity", {
+    ID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    Nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Dificultad: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5,
+      },
+    },
+    Duracion: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    Temporada: {
+      type: DataTypes.ENUM("Verano", "Invierno", "Otoño", "Primavera"),
+    },
+  });
+};
